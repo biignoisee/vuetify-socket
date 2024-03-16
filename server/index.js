1dce6fa3eb6f62248ea9530b85fddc7c8d924783
biignoisee <omargabriel.aguilarmoscoso@gmail.com>
2024-03-16 10:59:57 -0500

    set up express socket

--- server/index.js
+++ server/index.js
@@ -1,1 +1,25 @@
-console.log("test")
+import express from 'express'
+import { Server } from 'socket.io'
+import { createServer } from 'http'
+import cors from 'cors'
+
+const app = express()
+
+const server = createServer(app)
+const io = new Server(server, {
+  cors:{
+    origin: 'http://localhost:3000/',
+    methods: ['GET', 'POST'],
+    credentials: true
+  }
+})
+
+io.on('connection', (socket) => {
+  console.log("WS connected", socket.id);
+});
+
+const PORT = 30001 || process.env.PORT
+
+server.listen(PORT, () => {
+  console.log("Server is running");
+})
