eeb347a6d83cf436063b7b4c5ee124ca0ea2e830
biignoisee <omargabriel.aguilarmoscoso@gmail.com>
2024-03-18 11:06:44 -0500

    set up express socket

--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -1,7 +1,14 @@
 <template>
-  <HelloWorld />
+  <div>Test server</div>
 </template>
 
 <script lang="ts" setup>
-  //
+import {ref, onMounted, onBeforeUnmount} from 'vue'
+import {io, type Socket} from 'socket.io-client'
+
+const socket = ref<Socket>()
+
+onMounted(() => {
+  socket.value = io()
+})
 </script>
