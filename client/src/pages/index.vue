949f97871a528124ddefafab74f1a4dca583ec2c
biignoisee <omargabriel.aguilarmoscoso@gmail.com>
2024-03-18 11:22:42 -0500

    vuetify vtemplate

--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -1,14 +1,57 @@
 <template>
-  <div>Test server</div>
+  <VContainer>
+    <VForm @submit.prevent="onSubmit">
+      <VCard max-width="600" class="mx-auto pa-5">
+        <VCardTitle>
+          <div class="d-flex align-center justify-center">
+            <VIcon icon="mdi-chat" />
+            <h3 class="ml-2">Vue Chatapp</h3>
+          </div>
+        </VCardTitle>
+        <VCardText class="py-4">
+          <VTextField label="Username" v-model="state.username"></VTextField>
+          <VSelect :items="rooms" label="Room" v-model="state.room"></VSelect>
+        </VCardText>
+        <VCardActions>
+          <VBtn
+            block
+            color="primary"
+            :disabled="!state.username || !state.room"
+            variant="outlined"
+            type="submit"
+          >
+            Join Chatapp
+          </VBtn>
+        </VCardActions>
+      </VCard>
+    </VForm>
+  </VContainer>
 </template>
 
 <script lang="ts" setup>
-import {ref, onMounted, onBeforeUnmount} from 'vue'
-import {io, type Socket} from 'socket.io-client'
+import { reactive, ref, onMounted, onBeforeUnmount } from "vue";
+import { useRouter } from "vue-router";
+const router = useRouter();
+const rooms = ["weighted calisthenic", "streetlifting", "sft", "testpe"];
+const state = reactive({
+  username: "",
+  room: rooms[0],
+});
 
-const socket = ref<Socket>()
+const onSubmit = () => {
+  console.log('[SUBMIT]')
+  router.push(`/chat?username=${state.username}&room=${state.room}`)
+}
+
+import { io, type Socket } from "socket.io-client";
+
+const socket = ref<Socket>();
 
 onMounted(() => {
-  socket.value = io('http://localhost:3001')
-})
+  socket.value = io("http://localhost:3001");
+});
+onBeforeUnmount(() => {
+  console.log("[DISCONNECT_BLOCK]");
+  socket.value?.disconnect();
+});
 </script>
